080b835abb5ddff1b193c6ef16ecee4491d866aa
laetitiamoyon <laetitia.moyon.pro@gmail.com>
2022-01-28 23:27:29 +0100

    tennis

---
+++ tennis/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <Tennis></Tennis>
+  </div>
+</template>
+
+<script>
+
+import Tennis from './components/Tennis.vue'
+export default {
+  name: 'App',
+  components: {
+    Tennis
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+body {
+  margin: 0;
+  font-family: 'Roboto', sans-serif;
+  background: url("https://www.enghienlesbains.fr/sites/default/files/media/images/shutterstock_1681159513.jpg") no-repeat center center / cover;
+  background-repeat: repeat-y;
+  height: 100%;
+  min-height: 700px;
+  padding : 32px 62px;
+}
+
+@media (max-width: 600px)
+{
+  body { padding : 32px; }
+}
+</style>
---
+++ tennis/src/components/Tennis.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1 class="title">Les joueurs de tennis</h1>
+    <div class="container">
+      <div v-for="player in players" :key="player.id" class="card">
+        <div class="fullName">{{ player.firstname }} {{ player.lastname }}</div>
+        <img class="image" :src="player.picture" alt=""/>
+        <div class="rank">Rang {{player.data.rank}}</div>
+        <button class="button">Plus d'information</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data () {
+    return {
+      players : [],
+    }
+    
+  },
+  methods: {
+    getPlayerData () {
+      axios.get('https://latelierssl.blob.core.windows.net/trainingday/TennisStats/headtohead.json')
+          .then(response => this.players =  response.data.players)
+    }
+  },
+  mounted() {
+    this.getPlayerData()
+  },
+  
+}
+</script>
+
+<style src="./tennis.css"></style>
