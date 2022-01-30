c3c4960516f740105d515673edfd97b7f1bec2d2
laetitiamoyon <laetitia.moyon.pro@gmail.com>
2022-01-30 20:06:12 +0100

    wip

--- tennis/src/domains/tennis/components/PlayerInformation/PlayerInformation.vue
+++ tennis/src/domains/tennis/components/PlayerInformation/PlayerInformation.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
     <div class="container">
-      <div class="card">
+      <div class="player">
         <div class="title">{{ player.firstname }} {{ player.lastname }}</div>
         <img class="image" :src="player.picture" alt=""/>
-
         <div><div class="label">Rang :</div> {{player.data.rank}}</div>
         <div><div class="label">Age :</div> {{player.data.age}} ans</div>
         <div><div class="label">Taille :</div> {{player.data.height}}cm</div>
         <div><div class="label">Poids :</div> {{player.data.weight / 1000}}kg</div>
-        <!-- <div><div class="label">Sex :</div> {{player.sex === 'M' ? 'Homme' : 'Femme'}}</div> -->
         <div><div class="label">Pays d'origine :</div></div>
         <img class="image" :src="player.country.picture" alt="player.country.code"/>
-
         <div><div class="label">Résultat des derniers matchs :</div> {{player.data.last.join(', ')}}</div>
         <div><div class="label">Nombre de points :</div> {{player.data.points}}</div>
       </div>
@@ -28,6 +25,7 @@ export default{
   data () {
     return { 
       id: this.$route.params.id,
+      players : [],
       player: []
     }
   },
@@ -35,7 +33,6 @@ export default{
     backToHome () {
       this.$router.push('/');
     },
-    
   },
   async mounted () {
     this.id = this.$route.params.id;
--- tennis/src/domains/tennis/components/Players/Players.vue
+++ tennis/src/domains/tennis/components/Players/Players.vue
@@ -2,13 +2,13 @@
   <div>
     <h1 class="title">Les joueurs de tennis</h1>
     <div class="cards" >
-      <div v-for="player in players" :key="player.id" class="card">
-        <div class="fullName">{{ player.firstname }} {{ player.lastname }}</div>
-        <img class="image" :src="player.picture" alt=""/>
-        <div class="rank">Rang {{player.data.rank}}</div>
-        <button class="button">
-          <router-link :to="{ name: 'PlayerInformation', params: {id: player.id}}">Voir plus d'informations</router-link>
-      </button>
+      <div v-for="bestPlayer in bestPlayers" :key="bestPlayer.id" class="card">
+        <div class="fullname">{{ bestPlayer.firstname }} {{ bestPlayer.lastname }}</div>
+        <img class="image" :src="bestPlayer.picture" alt=""/>
+        <div class="rank">Rang {{bestPlayer.data.rank}}</div>
+        <div>
+          <router-link class="button" :to="{ name: 'PlayerInformation', params: {id: bestPlayer.id}}">Voir plus d'informations</router-link>
+      </div>
       </div>
     </div>
   </div>
@@ -20,12 +20,13 @@ export default {
   data () {
     return {
       players : [],
+      bestPlayers : [],
     }
   },
   async mounted() {
    this.players = await getPlayers()
+   this.bestPlayers = this.players.sort((a,b) => a.data.rank - b.data.rank)
   },
-  
 }
 </script>
 <style src="./players.css"></style>
